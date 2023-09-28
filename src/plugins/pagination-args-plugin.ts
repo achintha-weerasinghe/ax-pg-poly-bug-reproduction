8aa615fd2d1a6cbbd45694a10fa65c7d3b0a5725
Achintha Weerasinghe <weerasinghe@axinom.com>
2023-09-28 09:55:58 +0530

    comment removed

--- src/plugins/pagination-args-plugin.ts
+++ src/plugins/pagination-args-plugin.ts
@@ -2,13 +2,6 @@ import { GraphQLError } from 'graphql';
 import { FieldArgs, lambda } from 'postgraphile/grafast';
 import { makeWrapPlansPlugin } from 'postgraphile/utils';
 
-/**
- * The validate function is used to validate the arguments passed to a GraphQL field. It takes in the field arguments,
- * a list of argument names to validate, and a validator function as inputs.
- * @param fieldArgs - The field arguments passed to the GraphQL field
- * @param args - A list of argument names to validate
- * @param validator - A function that performs the validation logic on each argument
- */
 const validate = (fieldArgs: FieldArgs, args: string[], validator: (arg: any) => void): void => {
   args.forEach((argName) => {
     const $checkArg = lambda(fieldArgs.getRaw(argName), validator);
